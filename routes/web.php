d640dfbadef23c74d6a18893bef9559e8efb0034
xiomarau <urxiomara6@gmail.com>
2024-11-06 23:04:49 -0500

    rutas nuevas

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::get('/product/{id}/editar', [ProductController::class, 'edit'])->name('product.edit');
 
 
-Route::get('/products',[ProductController::class,'index'])->name('product.index');
-Route::get('/product/create',[ProductController::class,'create']);
-Route::post('/product/store',[ProductController::class,'store'])->name('product.store');
-Route::get('product/{id}',[ProductController::class,'show'])->name('product.show');
-Route::put('product/{id}',[ProductController::class,'update'])->name('product.update');
-Route::delete('product/{id}',[ProductController::class,'destroy'])->name('product.destroy');//estaaaaaa
-Route::get('product/{id}/editar',[ProductController::class,'edit'])->name('product.edit');
 
 
 
