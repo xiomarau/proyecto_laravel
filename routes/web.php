9231639cfbccfcc634306d9c9eb34bd8d9f504a3
xiomarau <urxiomara6@gmail.com>
2024-11-06 22:54:33 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-use App\Http\Controllers\QuadraticController;
 
-Route::get('/quadratic', [QuadraticController::class, 'index']);
-Route::post('/quadratic', [QuadraticController::class, 'solve']);
+
+Route::get('/products',[ProductController::class,'index'])->name('product.index');
+Route::get('/product/create',[ProductController::class,'create']);
+Route::post('/product/store',[ProductController::class,'store'])->name('product.store');
+Route::get('product/{id}',[ProductController::class,'show'])->name('product.show');
+Route::put('product/{id}',[ProductController::class,'update'])->name('product.update');
+Route::delete('product/{id}',[ProductController::class,'destroy'])->name('product.destroy');//estaaaaaa
+Route::get('product/{id}/editar',[ProductController::class,'edit'])->name('product.edit');
+
+
+
+
+
+
+
+
 
